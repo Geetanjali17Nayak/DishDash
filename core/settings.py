c70c09ffa623b2c3f180cedbcb701f1a6542af7a
Geetanjali17Nayak <117501359+Geetanjali17Nayak@users.noreply.github.com>
2024-10-21 11:58:27 +0530

    Initial commit

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
+EXTERNAL_APPS ={
+    'home',
+    'vege',
+}
+INSTALLED_APPS += EXTERNAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+     peoples=[{'name': "Geetanjali" , 'age': int('21')}, 
+	             {'name': "Ayush" , 'age':int('21')}]
+     
+     return render(request, "ind.html", context = {'peoples' : peoples})
+
